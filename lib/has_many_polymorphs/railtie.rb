9f18518429207579c43c09d46530dadba98cb154
Ben Hutton <benhutton@gmail.com>
2010-11-01 15:41:24 -0400

    Fixing spelling mistake.

--- lib/has_many_polymorphs/railtie.rb
+++ lib/has_many_polymorphs/railtie.rb
@@ -3,7 +3,7 @@ module HasManyPolymorphs
     initializer "has_many_polymorphs" do |app|
       require 'has_many_polymorphs/autoload'
       if defined?(Spork)
-        Spork.trap_class_methods(HasManyPolymorphs, :setup)
+        Spork.trap_class_method(HasManyPolymorphs, :setup)
       end
       HasManyPolymorphs.setup
     end
