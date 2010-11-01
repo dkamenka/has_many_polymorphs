79938207067fe8d9fcbf0f0d13fbc3d1c815f8bd
Ben Hutton <benhutton@gmail.com>
2010-11-01 14:37:53 -0400

    If using Spork, trap HasManyPolymorphs#setup

--- lib/has_many_polymorphs/railtie.rb
+++ lib/has_many_polymorphs/railtie.rb
@@ -2,6 +2,9 @@ module HasManyPolymorphs
   class Railtie < Rails::Railtie
     initializer "has_many_polymorphs" do |app|
       require 'has_many_polymorphs/autoload'
+      if defined?(Spork)
+        Spork.trap_class_methods(HasManyPolymorphs, :setup)
+      end
       HasManyPolymorphs.setup
     end
   end
